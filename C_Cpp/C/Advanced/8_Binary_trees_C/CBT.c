8b1d61d772ef8ab0e575ca5c7bdd8b98750e52bf
JJH0204 <jaeho6627@kakao.com>
2024-02-28 23:19:47 +0900

    Annotation Theorem

--- C_Cpp/C/Advanced/8_Binary_trees_C/CBT.c
+++ C_Cpp/C/Advanced/8_Binary_trees_C/CBT.c
@@ -1,6 +1,4 @@
 // Complete Binary Tree (CBT).c
-/*배열을 응용한 완전 이진 트리 구현*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,13 +9,6 @@ typedef struct Tree
     struct Tree *P_left;
     struct Tree *P_right;
 } BTree;
-
-/*Define a complete binary tree structure*/
-// typedef struct Complete_Binary_Tree
-// {
-//     Node_t * P_root;
-//     unsigned int _level;
-// } Tree;
 
 /*Define structures for Queue implementation*/
 typedef struct Queue
@@ -31,7 +22,6 @@ void push(Queue ** head, BTree *_val);
 BTree * pop(Queue ** head);
 
 /*Tree Function*/
-// Tree * new_tree(void);
 void insert(BTree **_tree, int _val);
 BTree **BFS(BTree *_root);
 void print_tree(BTree *_tree);
@@ -39,23 +29,17 @@ void print_tree(BTree *_tree);
 int main (void)
 {
     /*Tree Dynamic Assignment*/
-    // Tree * Binary_Tree = new_tree();
     BTree *root = NULL;
 
-    /*TODO: Insert data in to tree*/
-    insert(&root, 10);       // root Node data
-    // printf("%d", root->_val);
-    insert(&root, 20);       // left Node from root Node
-    // printf("%d", root->P_left->_val);
-    insert(&root, 30);       // right Node from root Node
-    // printf("%d", root->P_right->_val);
-    insert(&root, 40);       // After Repetition
-    printf("%d", root->P_left->P_left->_val);
+    /*Insert data in to tree*/
+    insert(&root, 10);       
+    insert(&root, 20);       
+    insert(&root, 30);       
+    insert(&root, 40);       
     insert(&root, 50);
-    printf("%d", root->P_left->P_right->_val);
-    // insert(&root, 60);
-    // insert(&root, 70);
-    // insert(&root, 80);
+    insert(&root, 60);
+    insert(&root, 70);
+    insert(&root, 80);
 
     /*TODO: Print all data of tree*/
     // print_tree(root);
@@ -109,23 +93,9 @@ BTree* pop(Queue ** _head)
 }
 
 /*Function of allocate new tree*/
-// Tree * new_tree(void)
-// {
-//     Tree * new_Tree = (Tree*)malloc(sizeof(Tree));
-//     new_Tree->P_root=NULL;
-//     new_Tree->_level=0;
-//     return new_Tree;
-// }
-
 /*Add Data Function*/
-/*Browse the tree and find the empty node.*/
-/*Browse rule*/
-// 1. 뿌리 레벨부터 순차적으로 탐색한다.
-// 2. 왼쪽 > 오른쪽 순으로 탐색한다.
-// 3. 아직 탐색을 완료하지 못한 레벨이 있을 경우 다음 레벨로 이동하지 않는다.
 void insert(BTree **_tree, int _val)
 {
-    // Queue * P_queue = NULL;
     BTree * newNode = NULL;
     BTree ** currentNode = NULL;
 
@@ -134,18 +104,15 @@ void insert(BTree **_tree, int _val)
         return;
     }
 
-    /*Create a Node*/
     newNode = (BTree *)malloc(sizeof(BTree));
     newNode->_val = _val;
     newNode->P_left = NULL;
     newNode->P_right = NULL;
 
-    /*case1: If the structure member variable P_root in the _tree points to Null*/
     if ((*_tree) == NULL)
     {
         (*_tree) = newNode;
     }
-    /*case2: If there is a node that P_root points to*/
     else
     {
         currentNode = BFS((*_tree));
@@ -154,7 +121,7 @@ void insert(BTree **_tree, int _val)
     return;
 }
 
-/*아이디어: 넓이 우선 탐색(Queue 자료구조 사용)*/
+/*Idea: Breadth-First Search (using Queue data structures)*/
 BTree **BFS(BTree *_root)
 {
     Queue *P_head = NULL;
@@ -185,8 +152,8 @@ BTree **BFS(BTree *_root)
     }
 }
 
-// void print_tree(Tree * _tree)
-// {
-
-//     return;
-// }
+void print_tree(BTree *_tree)
+{
+    /*TODO: 트리 출력 BST*/
+    return;
+}
