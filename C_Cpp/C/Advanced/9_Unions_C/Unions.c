a6fb9c1d7792ac77145aec08a89070fa0e6524e2
JJH0204 <jaeho6627@kakao.com>
2024-02-29 22:52:37 +0900

    Practice using unions

---
+++ C_Cpp/C/Advanced/9_Unions_C/Unions.c
@@ -0,0 +1,16 @@
+// Unions.c
+// Practice file to understand the concept of Unions in C language
+
+#include <stdio.h>
+
+/*Unions*/
+union case1
+{
+    int Int;
+    char Char;
+};
+
+int main(void)
+{
+    return 0;
+}
