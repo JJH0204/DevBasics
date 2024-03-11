85f2972e882bd35f3367220355fc783e138d3503
JJH0204 <jaeho6627@kakao.com>
2024-03-11 11:15:12 +0900

    doubly linked list(addData, getData)

--- Data_Structure_and_Algorithms/3_LinkedList_Add/DoubleLinkedList.c
+++ Data_Structure_and_Algorithms/3_LinkedList_Add/DoubleLinkedList.c
@@ -16,7 +16,7 @@ typedef struct DoubleLinkedList
     node sHeader;
 } dList;
 
-dList *createDoubleList (void)
+dList *createList (void)
 {
     dList *pResult = (dList*)malloc(sizeof(dList));
     if (pResult == NULL)
@@ -27,7 +27,9 @@ dList *createDoubleList (void)
 int addData(dList *_pList_, int _nData_, unsigned int _nIndex_)
 {
     node *pNewNode = NULL;
-    int nCount;
+    node *pCurrentNode = NULL;
+    node *pNextNode = NULL;
+    int nCount = 0;
     if (_pList_ == NULL)
         return -1;
     if ((_nIndex_ > _pList_->nCurrentCount) || (_nIndex_ < 0))
@@ -36,10 +38,58 @@ int addData(dList *_pList_, int _nData_, unsigned int _nIndex_)
     pNewNode = (node*)malloc(sizeof(node));
     pNewNode->nData = _nData_;
     // TODO: Create list node addition logic
-    /* code's */
-
+    /* code */
+    pCurrentNode = &(_pList_->sHeader);
+    if ((pCurrentNode->pRightLink == NULL) && (pCurrentNode->pLeftLink == NULL))
+    {
+        _pList_->sHeader.pRightLink = pNewNode;
+        _pList_->sHeader.pLeftLink = pNewNode;
+        pNewNode->pRightLink = pCurrentNode;
+        pNewNode->pLeftLink = pCurrentNode;
+    }
+    else
+    {
+        for ( ; nCount < _nIndex_; nCount++)
+            pCurrentNode = pCurrentNode->pRightLink;
+        
+        pNextNode = pCurrentNode->pRightLink;
+        pCurrentNode->pRightLink = pNewNode;
+        pNewNode->pLeftLink = pCurrentNode;
+        pNextNode->pLeftLink = pNewNode;
+        pNewNode->pRightLink = pNextNode;
+    }
+    _pList_->nCurrentCount++;
     return 0;
 }
+
+int getListData(dList *_pList_, int _nIndex_)
+{
+    int nBetter = 0;
+    int nCount = 0;
+    node *pCurrentNode = NULL;
+
+    if (_pList_ == NULL)
+        return -1;      // no list
+    if (_pList_->sHeader.pRightLink == NULL)
+        return -2;      // no data
+    if ((_nIndex_ > _pList_->nCurrentCount) || (_nIndex_ < 0))
+        return -3;      // error Index
+    
+    pCurrentNode = &(_pList_->sHeader);
+    nBetter = (_pList_->nCurrentCount - 1) - _nIndex_;
+
+    // It is better to move to the right
+    if (nBetter >= _nIndex_)
+        for ( ; nCount <= _nIndex_; nCount++)
+            pCurrentNode = pCurrentNode->pRightLink;
+    // It is better to move to the left
+    else
+        for ( ; nCount <= nBetter; nCount++)
+            pCurrentNode = pCurrentNode->pLeftLink;
+    
+    return pCurrentNode->nData;
+}
+
 
 int main(int argc, char *argv[])
 {
