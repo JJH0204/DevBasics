39e5fed9bec659308b15e51aaf6fe7a03427dc70
JJH0204 <jaeho6627@kakao.com>
2024-03-21 22:53:19 +0900

    Update TODO:

---
+++ DSNA/Linear/6_usingStack/InverseNotation.c
@@ -0,0 +1,11 @@
+// InverseNotation.c
+#include "Stack.h"
+
+// TODO: 후위 표기법을 이해하고 스택으로 계산하는 코드를 작성
+int main (int argc, char *argv[])
+{
+    int i = 0;
+
+    
+    return 0;
+}
---
+++ DSNA/Linear/6_usingStack/intermediateNotation.c
@@ -0,0 +1,9 @@
+// intermediateNotation.c
+#include "Stack.h"
+
+// TODO: 중위표기 법을 후위 표기법으로 변환하는 코드 작성
+int main (int argc, char * argv[])
+{
+
+    return 0;
+}
