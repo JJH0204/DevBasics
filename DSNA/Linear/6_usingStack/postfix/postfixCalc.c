af81347d4d49686aecc43c584ea096a0fa3ad5f5
JJH0204 <jaeho6627@kakao.com>
2024-03-22 13:17:20 +0900

    Update postfixCalc

--- DSNA/Linear/6_usingStack/postfix/postfixCalc.c
+++ DSNA/Linear/6_usingStack/postfix/postfixCalc.c
@@ -8,7 +8,7 @@ bool Calc(const TOKEN *_pTOKEN_, const int _nSize_)
 {
     int nCount = 0;
     STACK *pStack = NULL;
-    TOKEN token_A, token_B;
+    NODE *token_A = *token_B = NULL;
     TYPE type;
     if (ISNULL(_pTOKEN_) || _nSize_ <= 2) return true;
 
@@ -22,32 +22,24 @@ bool Calc(const TOKEN *_pTOKEN_, const int _nSize_)
         {
             push(pStack, _pTOKEN_[nCount]);
         }
-        else if (type == MULTIPLY)           // 곱하기
-        {
-            token_B = pop(pStack)->tData;
-            token_A = pop(pStack)->tData;
-            push(pStack, setToken(OPERAND, token_A.dValue * token_B.dValue));
-        }
-        else if (type == DIVIDE)        // 나누기
-        {
-            token_B = pop(pStack)->tData;
-            token_A = pop(pStack)->tData;
-            push(pStack, setToken(OPERAND, token_A.dValue / token_B.dValue));
-        }
-        else if (type == PLUS)          // 더하기
-        {
-            token_B = pop(pStack)->tData;
-            token_A = pop(pStack)->tData;
-            push(pStack, setToken(OPERAND, token_A.dValue + token_B.dValue));
-        }
-        else if (type == MINUS)         // 빼기
-        {
-            token_B = pop(pStack)->tData;
-            token_A = pop(pStack)->tData;
-            push(pStack, setToken(OPERAND, token_A.dValue - token_B.dValue));
+        else {
+            token_B = pop(pStack);
+            token_A = pop(pStack);
+            if (type == MULTIPLY)           // 곱하기
+                push(pStack, setToken(OPERAND, token_A->tData.dValue * token_B->tData.dValue));
+            else if (type == DIVIDE)        // 나누기
+                push(pStack, setToken(OPERAND, token_A->tData.dValue / token_B->tData.dValue));
+            else if (type == PLUS)          // 더하기
+                push(pStack, setToken(OPERAND, token_A->tData.dValue + token_B->tData.dValue));
+            else if (type == MINUS)         // 빼기
+                push(pStack, setToken(OPERAND, token_A->tData.dValue - token_B->tData.dValue));
+            else                            // 에러
+                break;
+            free(token_A);
+            free(token_B);
         }
     }
-    if (ISEMPTY(pStack))    return true;
+    if (ISEMPTY(pStack) || pStack->nCurrentCount > 1)    return true;
     printf("> Result: %lf\n", pStack->pTOP->tData.dValue);
     deleteStack(pStack);
     return false;
