cc8929bae7aad35ac9f46c8feb37920a912089f6
JJH0204 <jaeho6627@kakao.com>
2024-03-23 19:32:17 +0900

    Update Build

---
+++ build/CMakeFiles/postfixNotation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/notationStack.c.o"
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/notationStack.c.o.d"
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/postfixCalc.c.o"
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/postfixCalc.c.o.d"
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/postfixNotation.c.o"
+  "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/6_usingStack/postfix/postfixNotation.c.o.d"
+  "postfixNotation"
+  "postfixNotation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/postfixNotation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
