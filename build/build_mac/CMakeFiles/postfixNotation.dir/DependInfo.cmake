8e80880f8ca01e53040b336b69dc0807f52b15d7
JJH0204 <jaeho6627@kakao.com>
2024-03-24 20:43:28 +0900

    Update build_mac

---
+++ build/build_mac/CMakeFiles/postfixNotation.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/admin/Documents/GitHub/DevBasics/source/DSNA/Linear/notationStack.c" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/notationStack.c.o" "gcc" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/notationStack.c.o.d"
+  "/Users/admin/Documents/GitHub/DevBasics/source/DSNA/Linear/postfixCalc.c" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/postfixCalc.c.o" "gcc" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/postfixCalc.c.o.d"
+  "/Users/admin/Documents/GitHub/DevBasics/source/DSNA/Linear/postfixNotation.c" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/postfixNotation.c.o" "gcc" "CMakeFiles/postfixNotation.dir/source/DSNA/Linear/postfixNotation.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/build_mac/CMakeFiles/postfixNotation.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for postfixNotation.
+# This may be replaced when dependencies are built.
