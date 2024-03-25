f8ecf62fcb3b9a96b40870ada7be51262a010ddc
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2024-03-25 14:28:06 +0900

    mainPostfix.c Update

--- source/DSNA/Linear/mainPostfix.c
+++ source/DSNA/Linear/mainPostfix.c
@@ -9,26 +9,32 @@
 int main (int argc, char *argv[])
 {
     int nSize = 9;
+    double dResult = 0.0;
 
-    TOKEN *pToken = (TOKEN *)malloc(sizeof(TOKEN) * nSize);
-
-    pToken[0] = setToken(OPERAND, 2);
-    pToken[1] = setToken(MINUS, 0);
-    pToken[2] = setToken(LPAREN, 0);
-    pToken[3] = setToken(OPERAND, 3);
-    pToken[4] = setToken(PLUS, 0);
-    pToken[5] = setToken(OPERAND, 4);
-    pToken[6] = setToken(RPAREN, 0);
-    pToken[7] = setToken(MULTIPLY, 0);
-    pToken[8] = setToken(OPERAND, 5);
-
-    printf("Infix Expression: ");
-    displayNotation(pToken, nSize);
-    pToken = infix2postfix(pToken, nSize);
-    displayNotation(pToken, nSize);
-    if (ISNULL(pToken)) return -1;
-    Calc(pToken, nSize);
-
+    TOKEN *pInfixToken = (TOKEN *)malloc(sizeof(TOKEN) * nSize);
+    TOKEN *pPostfixToken = NULL;
+	
+    if (ISNULL(pInfixToken)) return -1;
+    
+    pInfixToken[0] = setToken(OPERAND, 2);
+    pInfixToken[1] = setToken(MINUS, 0);
+    pInfixToken[2] = setToken(LPAREN, 0);
+    pInfixToken[3] = setToken(OPERAND, 3);
+    pInfixToken[4] = setToken(PLUS, 0);
+    pInfixToken[5] = setToken(OPERAND, 4);
+    pInfixToken[6] = setToken(RPAREN, 0);
+    pInfixToken[7] = setToken(MULTIPLY, 0);
+    pInfixToken[8] = setToken(OPERAND, 5);
+    printf("Infix Expression: \n\t> ");
+    displayNotation(pInfixToken, nSize);
+    
+    pPostfixToken = infix2postfix(pToken, nSize);
+    if (ISNULL(pPostfixToken)) return -1;
+    printf("Postfix Expression: \n\t> ");
+    displayNotation(pPostfixToken, nSize);
+    
+    dResult = Calc(pPostfixToken, nSize);
+	printf("Calc result: \n\t> %0.1f\n", dResult);
     free(pToken);
     return 0;
 }
