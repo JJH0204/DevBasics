763df61a3f95c47ea4b98676c3192ad8881af2c7
JJH0204 <jaeho6627@kakao.com>
2024-05-15 18:51:30 +0900

    Build BitSet

--- build/build_win/CMakeFiles/Makefile.cmake
+++ build/build_win/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/INTSET.dir/DependInfo.cmake"
+  "CMakeFiles/BITSET.dir/DependInfo.cmake"
   )
