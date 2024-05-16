7434252fbe26159ad434474792b61c8f56e42b4e
JJH0204 <jaeho6627@kakao.com>
2024-05-16 21:51:34 +0900

    Build Text Search Algorithm

--- build/build_win/CMakeFiles/Makefile.cmake
+++ build/build_win/CMakeFiles/Makefile.cmake
@@ -44,5 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/BITSET.dir/DependInfo.cmake"
+  "CMakeFiles/TextSearch.dir/DependInfo.cmake"
   )
