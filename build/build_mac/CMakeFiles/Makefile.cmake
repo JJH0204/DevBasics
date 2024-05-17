c71beeedb35cf0c33d868c4dcd32171ebb3b794b
JJH0204 <jaeho6627@kakao.com>
2024-05-17 11:18:24 +0900

    Build ChainHashing

--- build/build_mac/CMakeFiles/Makefile.cmake
+++ build/build_mac/CMakeFiles/Makefile.cmake
@@ -42,5 +42,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/TextSearch.dir/DependInfo.cmake"
+  "CMakeFiles/ChainHashing.dir/DependInfo.cmake"
   )
